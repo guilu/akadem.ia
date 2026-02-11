31aedc0c3937ecbffeeb0bbe60319695403095d9
guilu-klw <noreply@backendtothefuture.com>
2026-02-11 22:32:12 +0100

    ♻️ refactor (frontend) : ajustar email demo

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { apiBase } from '../api';
 
 export default function Register({ onToken }:{ onToken: (t:string)=>void }){
-  const [email, setEmail] = useState('demo@akdemya');
+  const [email, setEmail] = useState('demo@akdemya.com');
   const [password, setPassword] = useState('demo1234');
   const [confirmPassword, setConfirmPassword] = useState('demo1234');
   const [firstName, setFirstName] = useState('');
