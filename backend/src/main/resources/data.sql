b68a16f0bbb051e83b175abf34841fe4fe73767c
guilu-klw <noreply@backendtothefuture.com>
2026-02-14 09:56:42 +0100

    ✨ feat (backend) : usuario demo estudiante

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -26,7 +26,8 @@ ON CONFLICT DO NOTHING;
 
 -- ============ USERS ============
 INSERT INTO users(id, email, password_hash, role, first_name, last_name, occupation) VALUES
-  ('22222222-2222-2222-2222-222222222222','demo@akdemya.com','$2b$05$ZQaA2oiJaXCVFN540AU1du73.MrLGBMoacIKQ/.IJaDc2samK3eum','ADMIN',NULL,NULL,NULL)
+  ('22222222-2222-2222-2222-222222222222','demo@akdemya.com','$2b$05$ZQaA2oiJaXCVFN540AU1du73.MrLGBMoacIKQ/.IJaDc2samK3eum','ADMIN',NULL,NULL,NULL),
+  ('33333333-3333-3333-3333-333333333333','student@akdemya.com','$2b$05$ZQaA2oiJaXCVFN540AU1du73.MrLGBMoacIKQ/.IJaDc2samK3eum','STUDENT',NULL,NULL,NULL)
 ON CONFLICT DO NOTHING;
 
 -- ============ ANSWERS ============
